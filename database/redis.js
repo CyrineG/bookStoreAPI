40fbb033989481809c14829fe2bf84d16774dadb
CyrineG <syrynegamoudi@gmail.com>
2022-06-28 15:48:52 +0100

    Fix redis connection

--- database/redis.js
+++ database/redis.js
@@ -1,5 +1,7 @@
 const redis = require('redis');
-
-const client = redis.createClient();
+const client = redis.createClient({
+  url: 'redis://redis',
+  port: '6333',
+});
 
 module.exports = client;
